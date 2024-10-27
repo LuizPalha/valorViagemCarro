47de92a821ff542822476eb328ccdec7ce54cc8e
luizpalha <luizpalhaneto@hormail.com>
2024-10-26 22:55:19 -0300

    consumoCarro

---
+++ aula.js
@@ -0,0 +1,6 @@
+let precoCombust = 6.30
+let consumoCarro = 12 //Km por litros de gasolina 
+let distViagem = 10000
+let total =   (distViagem / consumoCarro) * precoCombust
+
+console.log ("o gasto total de combustivel da viagem e de R$ " + total.toFixed(2))
