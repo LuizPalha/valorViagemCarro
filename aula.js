b0cb8844fdf7fbddd169e76bfb228c534e80a971
luizpalha <luizpalhaneto@hormail.com>
2024-10-27 21:56:30 -0300

    consumoCarro

--- aula.js
+++ aula.js
@@ -1,6 +1,15 @@
-let precoCombust = 6.30
-let consumoCarro = 12 //Km por litros de gasolina 
-let distViagem = 10000
-let total =   (distViagem / consumoCarro) * precoCombust
+let consumoCarroGas = 12 //Km por litros de gasolina 
+let consumoCarroEt= 8 // Km por litros de etanol
+let distViagem = 1000
+let precoEtanol = 4.80
+let precoGasolina = 6.30
+let consumoCarro = 0
+let totalGas =   (distViagem / consumoCarroGas) * precoGasolina
+let totalEt = (distViagem / consumoCarroEt) * precoEtanol
 
-console.log ("o gasto total de combustivel da viagem e de R$ " + total.toFixed(2))
+if (consumoCarroGas){
+    console.log ("o gasto total de combustivel da viagem em gasolina e de R$ " + totalGas.toFixed(2))
+} 
+     if (consumoCarroEt) {
+        console.log ("o gasto total de combustivel da viagem em Etanol e de R$ " + totalEt.toFixed(2))
+    }
